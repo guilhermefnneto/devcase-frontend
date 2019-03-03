606f88483176893e9b14a437fc26effb737e3105
guilhermefnneto <guilherme.fn.neto@outlook.com>
2019-03-03 12:51:11 -0300

    Criando a interface da pontuação.

--- src/app/pontuacao/pontuacao.component.ts
+++ src/app/pontuacao/pontuacao.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
+import { Pontuacao } from './pontuacao'
+
 @Component({
     selector: 'dev-pontuacao',
     templateUrl: './pontuacao.component.html',
     styleUrls: [ './pontuacao.component.css' ]
 })
 export class PontuacaoComponent {
+
+    pontuacao: Pontuacao = null;
 
     
 
